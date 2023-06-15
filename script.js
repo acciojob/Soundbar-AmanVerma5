286f55a1ff06a13c68ef76640386edcd965bb4bf
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-15 17:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let btns=document.getElementsByTagName("button");
+
+for(let i=0;i<btns.length-1;i++){
+  //console.log(btns[i].innerText)
+  btns[i].addEventListener("click",playSound)
+}
+btns[btns.length-1].addEventListener("click",stopSound)
+
+function playSound(){
+  stopSound();
+  let audio=document.getElementById(this.innerText);
+  audio.play();
+}
+
+function stopSound(){
+  for(var i=0;i<btns.length-1;i++){
+    const audio = document.getElementById(btns[i].innerText)
+    audio.pause()
+    audio.currentTime = 0
+  }
+}
